db0a014cedbf9a1c8ba10f34dccfce1841dbf023
Lovre Nenadic <lovre.nenadic@gmail.com>
2025-11-18 22:15:16 +0100

    zadaci 1-5

--- rs_zadace/zadaca_3/zadatak5.py
+++ rs_zadace/zadaca_3/zadatak5.py
@@ -3,7 +3,7 @@ import asyncio
 podaci = [
     {'prezime': 'Anić', 'broj_kartice': '4242424242424242', 'CVV': '123'},
     {'prezime': 'Perić', 'broj_kartice': '5105105105105100', 'CVV': '321'},
-    {'prezime': 'Jurić', 'broj_kartice': '6011000990139425', 'CVV': '231'}
+    {'prezime': 'Jurić', 'broj_kartice': '6011000990139424', 'CVV': '231'}
 ]
 
 async def secure_data(osoba):
